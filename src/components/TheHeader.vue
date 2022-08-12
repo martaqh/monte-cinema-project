433ae9c0079b4d3f2dfd6578d081ff03310dcf4d
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 13:29:26 +0200

    Add TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<script>
+import { defineComponent } from 'vue';
+import TopNavbar from './TopNavbar.vue';
+import SignSection from './SignSection.vue';
+    
+
+    export default defineComponent({
+    components: { TopNavbar, SignSection }
+});
+</script>
+
+<template>
+    <div class="header">
+        <a href="/"><img src="@/assets/MonterailLogo.svg" class="logo" alt="Monterail logo" /></a>
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/settings.scss";
+
+.header {
+    height: 112px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 1;
+}
+
+</style>
