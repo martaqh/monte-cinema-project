855179bba9dfb49411d971b79ed3ead3ff026e81
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 13:29:01 +0200

    Add SignSection component

---
+++ src/components/SignSection.vue
@@ -0,0 +1,30 @@
+<script>
+    import { defineComponent } from 'vue';
+import ButtonlikeLink from './common/ButtonlikeLink.vue';
+
+    export default defineComponent({
+    components: { ButtonlikeLink }
+});
+</script>
+
+<template>
+    <div class="sign-section">
+        <ButtonlikeLink class="tertiary">Register</ButtonlikeLink>
+        <ButtonlikeLink class="primary">Log in</ButtonlikeLink>  
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/settings.scss";
+
+.sign-section {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 345px;
+    height: 100%;
+    font-family: $font-mono;
+    font-size: 16px;
+}
+
+</style>
