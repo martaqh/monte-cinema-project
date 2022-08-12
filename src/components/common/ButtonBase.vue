1a63acdae9fcd52c990ef58aadd984179beb81a6
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 13:26:21 +0200

    Add ButtonBase component

---
+++ src/components/common/ButtonBase.vue
@@ -0,0 +1,38 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      
+    }
+  },
+  
+});
+
+</script>
+
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+
+</template>
+
+<style lang="scss" scoped>
+
+@import "@/assets/styles/settings.scss";
+
+.button {
+  height: 40px;
+  width: 100px;
+  border-radius: 20px;
+  
+}
+.primary {
+  background-color: $color-brand;
+};
+
+
+</style>
+
