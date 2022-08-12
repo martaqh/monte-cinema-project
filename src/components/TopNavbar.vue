ea6a261b206f25690e7900608d2cb2fed32e14cc
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 13:28:30 +0200

    Add TopNavbar and TopNavbarTab components

---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,30 @@
+<script>
+    import { defineComponent } from 'vue';
+import TopNavbarTab from './TopNavbarTab.vue';
+    export default defineComponent({
+    components: { TopNavbarTab }
+});
+</script>
+
+<template>
+    <div class="top-nav-bar">
+        <TopNavbarTab>Movies</TopNavbarTab>
+        <TopNavbarTab>Screenings</TopNavbarTab>
+        <TopNavbarTab>Contact us</TopNavbarTab>     
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/settings.scss";
+
+.top-nav-bar {
+    display: flex;
+    width: 424px;
+    height: 100%;
+    align-items: center;
+    font-family: $font-main;
+    font-size: 18px;
+    color: $color-text-navbar;
+}
+
+</style>
---
+++ src/components/TopNavbarTab.vue
@@ -0,0 +1,28 @@
+<script>
+import { defineComponent } from 'vue';
+
+    
+
+    export default defineComponent({
+    
+});
+</script>
+
+<template>
+    <div class="top-navbar-tab">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/settings.scss";
+
+.top-navbar-tab {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 33%;
+}
+
+</style>
