7c4a6d6640341fc368f682e0ff2eee87d0e32d07
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 13:29:56 +0200

    Add TheContainer component

---
+++ src/components/common/TheContainer.vue
@@ -0,0 +1,20 @@
+<script>
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+    });
+</script>
+
+<template>
+    <div class="container">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/settings.scss";
+
+.container {
+    margin: 0 48px;
+}
+
+</style>
