32e96a9206b5c53a8348d4d383a59ede24b68963
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 13:26:39 +0200

    Add ButtonlikeLink component

---
+++ src/components/common/ButtonlikeLink.vue
@@ -0,0 +1,55 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      
+    }
+  },
+  
+});
+
+</script>
+
+<template>
+  <div class="buttonlike-link">
+    <a>
+      <slot></slot>
+    </a>
+  </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+@import "@/assets/styles/settings.scss";
+
+.buttonlike-link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 114px;
+    height: 40px;
+    border-radius: $radius-button;
+  
+    
+}
+
+.primary {
+  background-color: $color-brand;
+  color: $color-text-reverse;
+}
+
+.secondary {
+  background-color: $color-text-main;
+}
+
+.tertiary {
+  border: none;
+  color: $color-brand;
+}
+
+
+
+</style>
