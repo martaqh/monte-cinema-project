0219ffc91bcc2b28e242e5d4819a2edfd92d3c74
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-12 22:55:50 +0200

    Add TheTag component

---
+++ src/components/common/TheTag.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    });
+</script>
+
+<template>
+    <div class="tag">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.tag {
+    width: 73px;
+    height: 32px;
+    background: $color-tag-background;
+    border-radius: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: $color-text-label;
+    font-family: $font-main;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+}
+
+</style>
