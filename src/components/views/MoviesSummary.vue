f39dc5abf49a602ea42acbaa78a6bbe59672009c
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-13 14:13:06 +0200

    Add MoviesSummary component

---
+++ src/components/views/MoviesSummary.vue
@@ -0,0 +1,56 @@
+<script>
+import { defineComponent } from 'vue';
+import TheTag from '../common/TheTag.vue';
+import SectionTitle from '../common/SectionTitle.vue';
+import MovieCard from './MovieCard.vue';
+
+export default defineComponent({
+    components: { MovieCard }
+});
+</script>
+
+<template>
+    <div class="movies-summary-header">
+        <p>soon in the cinema</p>
+        <a>see all</a>
+    </div>
+    <div class="movies-summary">
+        <MovieCard />
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.movies-summary {
+    display: flex;
+    justify-content: space-between;
+    margin: 64px 0;
+}
+
+.movies-summary-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    font-family: $font-mono;
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 170%;
+    letter-spacing: 0.015em;
+    text-transform: uppercase;
+    margin-top: 64px;
+
+    p {
+        color: $color-text-main;
+    }
+
+    a {
+        color: $color-brand;
+    }
+
+
+}
+
+
+
+</style>
