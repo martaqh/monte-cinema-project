a97f34e44d9c09044a2c2dbcc3dac17bee20b348
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-19 13:49:47 +0200

    Set up eslint, prettier and editor

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,26 @@ module.exports = {
   "root": true,
   "extends": [
     "plugin:vue/vue3-essential",
-    "eslint:recommended"
+    "eslint:recommended",
+    "prettier",
+    "plugin:vue/essential"
   ],
   "env": {
     "node": true,
     "browser": true,
     "es6": true,
   },
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "indent": [
+        "error",
+        2
+    ],
+  }
 }
