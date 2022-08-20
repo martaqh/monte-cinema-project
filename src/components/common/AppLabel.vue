9c7b3048c48d18941e9fa386c8aaeb681f7e827a
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-20 22:57:28 +0200

    Add AppLabel component

---
+++ src/components/common/AppLabel.vue
@@ -0,0 +1,25 @@
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    });
+</script>
+
+<template>
+    <div class="label">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.label {
+    font-family: $font-mono;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    text-transform: uppercase;
+    color: $color-text-label;
+    margin-bottom: 12px;
+}
+</style>
