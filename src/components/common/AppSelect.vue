8818bd5bad970ddc3f18160ccfd3df9cfecb7832
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-20 22:58:18 +0200

    Add AppSelect component

---
+++ src/components/common/AppSelect.vue
@@ -0,0 +1,26 @@
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+});
+</script>
+
+<template>
+  <select class="select">
+    <option selected>All movies</option>
+    <option>Star Wars</option>
+    <option>Predator</option>
+  </select>
+</template>
+
+<style lang="scss" scoped>
+.select {
+  padding: 24px 16px 24px 24px;
+  gap: 10px;
+  background: $color-field-background;
+  border-radius: 8px;
+  border: none;
+  width: 100%;
+  height: fit-content;
+  font-size: 18px;
+}
+</style>
