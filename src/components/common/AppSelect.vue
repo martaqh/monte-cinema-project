569ff7d1aa323a9acebead7485a939691cbaf829
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-21 15:53:55 +0200

    Refactor AppSelect component

--- src/components/common/AppSelect.vue
+++ src/components/common/AppSelect.vue
@@ -1,20 +1,25 @@
 <script>
 import { defineComponent } from 'vue';
 export default defineComponent({
+  data() {
+    return {
+      optionSelected: 'All movies',
+      options: ['Predator', 'Star Wars', 'Harry Potter']
+    }
+  }
 });
 </script>
 
 <template>
   <select class="select">
-    <option selected>All movies</option>
-    <option>Star Wars</option>
-    <option>Predator</option>
+    <option selected>{{this.optionSelected}}</option>
+    <option v-for="option of options">{{ option }}</option>
   </select>
 </template>
 
 <style lang="scss" scoped>
 .select {
-  padding: 24px 16px 24px 24px;
+  padding: 1rem 2rem;
   gap: 10px;
   background: $color-field-background;
   border-radius: 8px;
