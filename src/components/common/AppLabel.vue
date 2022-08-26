efa5d851da99c26a2ff630b4a8d6b16bf81bb38e
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-26 16:57:16 +0200

    Fix label styling issue

--- src/components/common/AppLabel.vue
+++ src/components/common/AppLabel.vue
@@ -6,13 +6,14 @@
 
 <style lang="scss" scoped>
 .label {
-    font-family: $font-mono;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 18px;
-    text-transform: uppercase;
-    color: $color-text-label;
-    margin-bottom: 12px;
-    width: fit-content;
+  display: block;
+  font-family: $font-mono;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  text-transform: uppercase;
+  color: $color-text-label;
+  margin-bottom: 12px;
+  width: fit-content;
 }
 </style>
