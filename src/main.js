48bc078385e5402eb6c82aa5e900e5d9f45bf2ca
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-26 12:00:21 +0200

    Delete global use of vue date picker

--- src/main.js
+++ src/main.js
@@ -4,14 +4,10 @@ import App from './App.vue'
 import './styles/common/index.scss'
 import './styles/global/index.scss'
 
-import Datepicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css';
-
 import router from'@/router';
 
 const app = createApp(App);
 
 app.use(router)
-app.component('Datepicker', Datepicker);
 
 app.mount('#app')
