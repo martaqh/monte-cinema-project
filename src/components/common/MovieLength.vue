2f048d36c591c4a29e6ee3b5503151a87e1dac82
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-27 15:00:28 +0200

    Extract MovieLength component

---
+++ src/components/common/MovieLength.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    lengthInMinutes: {
+      type: Number,
+      required: true,
+    }
+  },
+  methods: {
+    lengthInHours(lengthInMinutes) {
+      const hours = Math.floor(lengthInMinutes / 60);
+      const minutes = `0${lengthInMinutes % 60}`.slice(-2);
+      return `${hours} h ${minutes} min`;
+    }
+  },
+})
+</script>
+
+<template>
+  <p class="movie-length">
+    {{ lengthInHours(lengthInMinutes) }}
+  </p>
+</template>
+
+<style lang="scss" scoped>
+.movie-length {
+    font-family: $font-main;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    color: $color-text-light;
+}
+</style>
