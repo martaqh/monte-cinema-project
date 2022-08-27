b1bd7fe5a15095293b52d3b4b702a67c63852f6e
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-27 23:17:20 +0200

    Add ScreeningsList component

---
+++ src/components/views/ScreeningsList.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from 'vue';
+import ScreeningsCard from './ScreeningsCard.vue';
+export default defineComponent({
+  components: { ScreeningsCard },
+  props: {
+    movies: {
+      type: Array,
+      required: true,
+    }
+  },
+  mounted() {
+    console.log(this.movies)
+  }
+})
+</script>
+
+<template>
+  <div class="screenings-list">
+    <ScreeningsCard v-for="movie in movies" :movieData="movie" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.screeinigs-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
