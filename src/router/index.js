8ea058cea8e0046fabcc8644efe8bce43f7ced53
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-30 22:39:11 +0200

    Remove unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,24 +7,6 @@ const routes = [
         component: () => import('@/components/pages/LandingPage.vue'),
     },
     {
-        path: '/screenings',
-        name: 'Screenings',
-        component: () => import('@/components/views/ScreeningsFilters.vue'),
-    },
-
-    {
-        path: '/movies',
-        name: 'Movies',
-        component: () => import ('@/components/views/MoviesSummary.vue'),
-    },
-  /*  {
-        path: '/movies/:movieId',
-        name: 'MoviePage',
-        props: true,
-        component: () => import ('@/components/views/MovieCard.vue'),
-
-    }, */
-    {
         path: '/:path*',
         redirect: '/no-match'
     },
