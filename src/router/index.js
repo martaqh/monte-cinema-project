5d819cac5ef3fb5eaa14c1eef1e2f10ed17e3a8b
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-08-30 22:23:29 +0200

    Remove unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,32 +7,14 @@ const routes = [
         component: () => import('@/components/pages/LandingPage.vue'),
     },
     {
-        path: '/screenings',
-        name: 'Screenings',
-        component: () => import('@/components/views/ScreeningsFilters.vue'),
-    },
-
-    {
-        path: '/movies',
-        name: 'Movies',
-        component: () => import ('@/components/views/MoviesSummary.vue'),
-    },
-  /*  {
-        path: '/movies/:movieId',
-        name: 'MoviePage',
-        props: true,
-        component: () => import ('@/components/views/MovieCard.vue'),
-
-    }, */
-    {
-        path: '/:path*',
-        redirect: '/no-match'
-    },
-    {
         path: '/no-match',
         name: 'NoMatch',
         component: () => import ('@/components/pages/NoMatch.vue'),
-    }
+    },
+    {
+      path: '/:path*',
+      redirect: '/no-match'
+    },
 ];
 
 const router = createRouter({
