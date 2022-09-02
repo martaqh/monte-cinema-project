1ebd94b0e8490a92191b2e8b3732877562cb4309
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-09-02 23:15:33 +0200

    Add AppInput component

---
+++ src/components/common/App/AppInput.vue
@@ -0,0 +1,60 @@
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    labelText: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      label: this.labelText
+    }
+  }
+
+});
+</script>
+
+<template>
+  <div class="input__wrapper">
+    <label class="input__label">{{label}}</label>
+    <input class="input"
+      :placeholder="placeholder"
+    />
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+.input__wrapper {
+  margin-top: 40px;
+}
+
+.input {
+  padding: 17px 24px;
+  gap: 10px;
+  background: $color-field-background;
+  border-radius: 8px;
+  border: none;
+  height: fit-content;
+  font-size: 18px;
+  width: 100%;
+}
+
+.input__label {
+    display: block;
+    font-family: $font-mono;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    text-transform: uppercase;
+    color: $color-text-label;
+    margin-bottom: 12px;
+    width: fit-content;
+}
+</style>
