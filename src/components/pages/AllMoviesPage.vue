5cd45ac414d3d085735d7cd731103c4893ae5244
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-09-05 14:58:50 +0200

    Add filtering movies by entered phrase

--- src/components/pages/AllMoviesPage.vue
+++ src/components/pages/AllMoviesPage.vue
@@ -7,12 +7,14 @@ import SectionTitle from '@/components/common/Section/SectionTitle.vue';
 import AppSelect from '@/components/common/App/AppSelect.vue';
 import MovieCard from '@/components/views/MovieCard.vue';
 import AppInput from '@/components/common/App/AppInput.vue';
+import AppLabel from '@/components/common/App/AppLabel.vue';
 
 export default defineComponent({
-  components: { BreadCrumbs, SectionTitle, AppSelect, MovieCard, AppInput },
+  components: { BreadCrumbs, SectionTitle, AppSelect, MovieCard, AppInput, AppLabel },
   data() {
     return {
-      optionSelected: 'All categories'
+      optionSelected: 'All categories',
+      inputContent: '',
     }
   },
   computed: {
@@ -20,8 +22,23 @@ export default defineComponent({
     moviesCategories() {
       const allCategories = this.movies.map(movie => movie.genre.name)
       return new Set(allCategories)
-    }
-  },
+    },
+    filteredMovies() {
+      if (this.optionSelected !== 'All categories') {
+        return this.movies.filter(movie => this.optionSelected === movie.genre.name)
+      } else {
+        return this.movies
+      }
+    },
+    moviesSearched() {
+      if (this.inputContent !== '') {
+        return this.filteredMovies.filter(movie => movie.title.toLowerCase().includes(this.inputContent.toLowerCase()))
+      } else {
+        return this.filteredMovies
+      }
+    },
+
+  }
 });
 </script>
 
@@ -30,20 +47,27 @@ export default defineComponent({
     <BreadCrumbs>Movies</BreadCrumbs>
     <SectionTitle size="large">All the movies</SectionTitle>
     <div class="all-movies__filters">
-      <AppInput
-        placeholder="What are you looking for?"
-        label-text="search"
-      />
-      <AppSelect
-        :options="moviesCategories"
-        v-model="optionSelected"
-        optionDefault="All categories"
-        label-text="category"
-      ></AppSelect>
+      <div class="all-movies__filters--select">
+        <p>{{inputContent}}</p>
+        <AppLabel>Search</AppLabel>
+        <AppInput
+          placeholder="What are you looking for?"
+          v-model="inputContent"
+        />
+      </div>
+      <div class="all-movies__filters--select">
+        <AppLabel>Category</AppLabel>
+        <AppSelect
+          :options="moviesCategories"
+          v-model="optionSelected"
+          optionStart="All categories"
+          label-text="category"
+        ></AppSelect>
+      </div>
     </div>
     <div class="all-movies__cards-list">
       <MovieCard
-        v-for="movie of movies"
+        v-for="movie of moviesSearched"
         :key="movie.id"
         :movie="movie"
       />
@@ -56,9 +80,16 @@ export default defineComponent({
   width: 100%;
 
   &__filters {
+    margin-top: 64px;
     display: grid;
     grid-template-columns: 2fr 1fr;
     grid-gap: 100px;
+
+    &--select, &--input {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+    }
 
     @include mobile {
       display: block;
