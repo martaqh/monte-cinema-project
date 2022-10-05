74949240c67d68d603e0bb33e8b7bc270955ad4d
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-10-05 13:26:19 +0200

    Set up typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
 /* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   "root": true,
   "extends": [
     "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript",
     "eslint:recommended",
     "prettier",
     "plugin:vue/essential"
